de48eb8dd8a0f9dcba695c05bfb0ac2324e112f2
Willem Meijer <w.meijer.5@student.rug.nl>
2022-01-24 18:06:59 +0100

    progress

--- scripts/satd_commit_pipeline/pipeline.py
+++ scripts/satd_commit_pipeline/pipeline.py
@@ -1,4 +1,4 @@
-from asyncio import subprocess
+from subprocess import Popen
 import json
 import os
 import requests
@@ -11,33 +11,34 @@ def read_input(proj_id: str) -> dict:
         return json.loads(input_file.read())
 
 
-def clone_repository( url: str, proj_name: str) -> str:
+def clone_repository(url: str, proj_name: str) -> str:
     dir = os.path.join("./repos", proj_name)
 
-    if not os.path.exists(dir):
-        Repo.clone_from(url, dir)
+    repo = Repo(dir) if os.path.exists(dir) else Repo.clone_from(url, dir)
 
-    return dir
+    return dir, repo
 
 
 def create_sq_project(sq_server: str, auth: Tuple, proj_id: str):
     url = f"{sq_server}/api/projects/create"
-    args = {"name": proj_id, "project": proj_id, "visibility": "public"}
+    name = f"project-{proj_id}"
+    args = {"name": name, "project": name, "visibility": "public"}
 
     requests.post(url=url, data=args, auth=auth)
 
-    return proj_id
+    return name
 
 
-def move_head(proj_dir: str, commit: dict):
+def move_head(proj_repo: Repo, commit: dict):
     hash = commit["sha"]
-    Repo.create_head(path=proj_dir, commit=hash, force=True)
+    proj_repo.git.checkout(hash)
+    # Repo.create_head (path=proj_dir, commit=hash, force=True)
 
 
 def run_sonarqube(sq_server: str, auth: Tuple, dir: str, proj_id: str):
     args = [
         "sonar-scanner",
-        f"-Dsonar.sources=./{dir}/",
+        "-Dsonar.sources=.",
         f"-Dsonar.projectKey={proj_id}",
         f"-Dsonar.host.url={sq_server}",
         f"-Dsonar.login={auth[0]}",
@@ -47,8 +48,12 @@ def run_sonarqube(sq_server: str, auth: Tuple, dir: str, proj_id: str):
         "-Dsonar.exclusions=/**.java",
     ]
 
-    prc = subprocess.run(args)
-    returncode = prc.returncode
+    os.chdir(dir)
+
+    with Popen(args) as run_sq_process:
+        returncode = run_sq_process.returncode
+
+    os.chdir("../" * (len(dir.split("/")) - 1))
 
     if returncode != 0:
         raise Exception(f"sq returned {returncode}...")
@@ -60,23 +65,25 @@ def export_issues(sq_server: str, auth: Tuple, proj_id: str, commit: dict):
     res = requests.get(url=url, data=data, auth=auth)
     sha = commit["sha"]
 
-    with open(f"./results/issues_proj-{proj_id}_commit-{sha}.json", "w") as output_file:
+    with open(f"./results/p-{proj_id}_c-{sha}.json", "w") as output_file:
         output_file.write(res.text)
 
 
 if __name__ == "__main__":
     git_id = "33884891"
     proj_uri = "https://github.com/apache/airflow"
+
     sq_server = "http://sonarqube:9000"
-    commits = read_input(git_id)
     sq_auth = ("admin", "password")
 
     proj_id = create_sq_project(sq_server, sq_auth, git_id)
-    proj_dir = clone_repository(proj_uri, git_id)
+    proj_dir, proj_repo = clone_repository(proj_uri, git_id)
+
+    commits = read_input(git_id)
 
     for commit in commits:
         print(f'handling {proj_id=}, {commit["sha"]=}')
-        
-        move_head(commit, proj_dir)
+
+        move_head(proj_repo, commit)
         run_sonarqube(sq_server, sq_auth, proj_dir, proj_id)
-        export_issues(sq_server, sq_auth, proj_id)
+        export_issues(sq_server, sq_auth, proj_id, commit)
