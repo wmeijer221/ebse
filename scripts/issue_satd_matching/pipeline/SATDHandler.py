bf7ff9d0bb6824f9238c4da4bfa32d376ed4a84a
Nik Dijkema <nikdijkema@hotmail.com>
2022-01-25 18:41:49 +0100

    Pipeline updated, pair-wise combinations between satd items and sq issues per commit per file

--- scripts/issue_satd_matching/pipeline/SATDHandler.py
+++ scripts/issue_satd_matching/pipeline/SATDHandler.py
@@ -19,3 +19,17 @@ class SATD:
       and item['satd_sha'] == commit_sha \
       and item['file'] == filename]
     return items
+
+  def getUniqueCommits(self):
+    items = list(set([item['satd_sha'] for item in self.data if item['file']]))
+    return items
+
+  def getUniqueFiles(self):
+    items = list(set([item['file'] for item in self.data if item['file']]))
+    return items
+
+  def getFilesByCommit(self, commit_sha):
+    items = list(set([item['file'] for item in self.data if item['file'] \
+      and 'satd_sha' in item.keys() \
+      and item['satd_sha'] == commit_sha]))
+    return items
--- scripts/issue_satd_matching/pipeline/pipeline.py
+++ scripts/issue_satd_matching/pipeline/pipeline.py
@@ -1,8 +1,10 @@
 import os
 import sys
 import json
+from itertools import product
 from SATDHandler import SATD
 from SQHandler import SQ
+from time import sleep
 
 
 SATD_FILE_PATH = '../results/airflow_line_numbers.json'
@@ -22,8 +24,28 @@ if __name__ == '__main__':
   satd = SATD(SATD_FILE_PATH)
   sq = SQ(SONARQUBE_FILES_PATH)
 
-  test_file = 'airflow/www/static/js/gantt.js'
-  test_sha = 'a9314dd63bc62d61ab7b625367f02650274ac99f'
+  unique_commits = satd.getUniqueCommits()
 
-  for x in sq.filterByCommitAndFilename(test_sha, test_file):
-    print(x)
+  # test_file = 'airflow/www/static/js/gantt.js'
+  # test_sha = 'a9314dd63bc62d61ab7b625367f02650274ac99f'
+
+  for commit in unique_commits:
+    # Fetch files with SATD in current commit
+    satd_files = satd.getFilesByCommit(commit)
+    for file in satd_files:
+      # Fetch the SATD items corresponding to the current commit and file
+      satd_items = satd.filterByCommitAndFilename(commit, file)
+      # Fetch SonarQube output for current commit and file
+      sonarqube_issues = sq.filterByCommitAndFilename(commit, file)
+
+      for comb in product(satd_items, sonarqube_issues):
+        # print("\n", comb)
+        # sys.stdout.flush()
+        # sleep(1)
+
+        # TODO:
+        # Comparison:
+        #  - Line distance (threshold parameter)
+        #  - Textual similarity scores
+        # Results:
+        #  - Format and store for manual analysis
